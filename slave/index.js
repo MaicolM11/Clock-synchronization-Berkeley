71ced91ee810b502be83ea8309daada39c375493
johan-martinez <johan.martinez01@uptc.edu.co>
2021-03-31 11:34:58 -0500

    Render de parte del Slave con método para cambiar la hora

--- slave/index.js
+++ slave/index.js
@@ -1,22 +1,21 @@
 const express = require('express')
-const io = require('socket.io')(http);
 const cors = require('cors')
 const path = require('path');
 var app = express()
-var port = process.env.PORT | 3000
-var http = require('http').createServer(app);
-
+var port = process.env.PORT | 3006
+var http = require('http').Server(app);
+const io = require('socket.io')(http);
 app.use(cors())
 app.use(express.json())
 app.use('/berkeley', require('./berkeley'))
 app.use(express.static(path.join(__dirname,'public')))
 
 // edit time from user
-app.post('time', (req, res)=>{
-
+app.post('/time', (req, res)=>{
+    res.sendStatus(200)
 })
 
-io.on('connection', (socket) => {
+io.sockets.on('connection', (socket) => {
     console.log('A new user connected');
     setInterval(() => {
         socket.emit('hours', `${clock.getHours()}:${clock.getMinutes()}:${clock.getSeconds()}`);
--- slave/src/components/App.js
+++ slave/src/components/App.js
@@ -1,20 +1,55 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import io from 'socket.io-client';
 
 
 function App (){
-    const [hour, setHour]=useState('')
-    let socket=io('/')
+    const [hour, setHour]=useState('-1:-1:-1')
+    const [val, setVal]=useState(false)
+    const [stop,setStop]=useState(0)
+    const [time,setTime]=useState('')
+
+    const changeTime=()=>{
+        if (time==""||time=='') {
+            alert('POR FAVOR INGRESE UNA HORA A ENVIAR')
+        }else{
+            fetch(`${window.location}time`, {
+                method: 'post',
+                body: JSON.stringify({time: time}),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((response) => {
+                document.getElementById('in-time').value=''
+            })
+        }
+    }
+    var socket=io('/',{autoConnect: false})
+
+    useEffect(()=>{
+        if (!val) {
+            socket.connect()
+            setVal(true)
+        }
+    },[stop])
+    
     socket.on('hours',(time)=>{
-        let event =window.event
-        if (event) event.preventDefault()
+        let event = window.event
+        if(event) event.preventDefault()
         setHour(time)
     })
     return (
             <div className="container mt-5">
                 <div className="row">
                     <div className="col">
-                        {hour}
+                        <h1 class="display-1">{hour}</h1>
+                        <h3>CAMBIAR LA HORA</h3>
+                        <input id="in-time" type='time' onChange={e=>{
+                            setTime(e.target.value)
+                        }}></input>
+                        <button className='btn btn-primary'
+                            onClick={changeTime}>ENVIAR</button>
                     </div>
                 </div>
             </div>
