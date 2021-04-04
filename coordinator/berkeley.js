253bf7cf96290abd20d29ee16295c6fdb1e21634
maicol <mmoarias4@gmail.com>
2021-04-03 19:28:28 -0500

    Ahora si

--- coordinator/berkeley.js
+++ coordinator/berkeley.js
@@ -1,37 +1,44 @@
 const route = require('express').Router()
 const axios = require('axios')
 const fs = require('fs')
+const logger = require('./logger')
 
 global.clock = new Date()
 
 setInterval(() => clock.setMilliseconds(clock.getMilliseconds() + 1000), 1000)
+setInterval(() => berkeley(), 60000)
 
-route.post('/', (req, res) => {
-    let allInstances = []
-    var url = `/berkeley/difference?hour=${clock.getHours()}&minutes=${clock.getMinutes()}&seconds=${clock.getSeconds()}`
-    getUrls().forEach(x => { allInstances.push(axios.get(x + url)) });
+function berkeley() {
+    let allInstances = [], urls = getUrls(),
+        path = `/berkeley/difference?hour=${clock.getHours()}&minutes=${clock.getMinutes()}&seconds=${clock.getSeconds()}`
+
+    urls.forEach(x => { allInstances.push(axios.get(x + path)) });
     axios.all(allInstances)
         .then(responseArr => {
-            let avg = 0, i = 0, urls = [];
+            let avg = 0, i = 0, allInstances = [];
             responseArr.forEach(x => avg += x.data.difference)
             avg /= (responseArr.length + 1)
-            getUrls().forEach(x => urls.push(
+            urls.forEach(x => allInstances.push(
                 axios.post(x + '/berkeley/time', {
                     adjustment: avg + responseArr[i++].data.difference
                 })))
-            axios.all(urls).catch((err) => { throw err })
+            axios.all(allInstances).catch((err) => { throw err })
             clock.setMilliseconds(clock.getMilliseconds() + avg * 1000)
-            res.sendStatus(200)
-        }).catch((err) => {
-            res.sendStatus(500)
-            console.log(err);
-        });
-})
+            doLogs(urls, responseArr, avg);
+        }).catch((err) => logger.error('[BERKELEY-ERROR] The algorithm has failed. '+  err.toString()));
+}
+
+function doLogs(urls, responseArr, avg) {
+    logger.info('[BERKELEY-INIT] server difference')
+    for (let i = 0; i < urls.length; i++) {
+        logger.info(`[BERKELEY-SERVER] ${urls[i]} ${responseArr[i].data.difference}`)
+    }
+    logger.info(`[BERKELEY-FINISH] The algorithm has finished successfully with AVG ${avg}`)
+}
 
 function getUrls() {
-    var urls = fs.readFileSync('./urls.txt', { encoding: "utf-8" }).split('\n')
-    urls.pop()
-    return urls
+    var values = JSON.parse(fs.readFileSync('./instances.json', { encoding: "utf-8" }).toString());
+    return values.filter(x => x.state).map(x => x.server)
 }
 
 module.exports = { route, getUrls }
--- coordinator/index.js
+++ coordinator/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const shell = require('shelljs');
+const logger = require('./logger')
 const fs = require('fs')
 const path = require('path');
 const axios = require('axios');
@@ -11,7 +12,10 @@ var http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 var port = 5000
-let portDocker = 4000;
+var portDocker = 4000;
+var path_instances = './instances.json';
+
+var getInstances = () => JSON.parse(fs.readFileSync(path_instances, { encoding: "utf-8" }).toString());
 
 app.use(cors())
 app.use(express.json())
@@ -20,39 +24,36 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 io.sockets.on('connection', (socket) => {
     setInterval(() => {
+        monitoring()
         socket.emit('hours', `${global.clock.toLocaleTimeString()}`);
-        sockets.emit('servers-data', monitoring());
+        socket.emit('servers-data', JSON.parse(fs.readFileSync(path_instances, { encoding: "utf-8" })));
     }, 1000);
 });
 
-function monitoring() {
-    var monitoring = []
-    var urls = berkeley.getUrls()
-    for (let i = 0; i < urls.length; i++) {
-        let server = urls[i]
-        let state = axios.get(`${server}/test`)
-            .then(() => true)
-            .catch(() => false)
-        monitoring.push({ server: server, state: await state })
+async function monitoring() {
+    var values = await getInstances()
+    for (let i = 0; i < values.length; i++) {
+        let server = values[i].server
+        let state = await axios.get(`${server}/test`).then(() => true).catch(() => false)
+        values[i].state = state
     }
-    return monitoring;
+    fs.writeFileSync(path_instances, JSON.stringify(values));
 }
-
 
 //metodo post para crear instancia
 app.post('/new_server', (req, res) => {
-    console.log('llego')
     try {
         portDocker++;
         shell.exec(`sh new_server.sh ${portDocker}`)
-        fs.appendFile('./urls.txt', `http://127.0.0.1:${portDocker}\n`, (err) => {
-            if (err) throw err;
-            console.log('new server has been added!');
-            res.sendStatus(200);
-        });
+        var values = getInstances()
+        values.push({ server: `http://127.0.0.1:${portDocker}`, state: true })
+        fs.writeFileSync(path_instances, JSON.stringify(values));
+        logger.info('New server has been added on port ' + portDocker);
+        res.sendStatus(200);
     } catch { res.sendStatus(500) }
 })
 
 http.listen(port, () => {
+
     console.log(`listening on port: ${port}`);
 });
